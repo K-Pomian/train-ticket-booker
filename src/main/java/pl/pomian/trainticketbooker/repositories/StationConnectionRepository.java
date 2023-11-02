79a3525e9e47bffbe1b7433ba15cd5b46bedc211
K-Pomian <pomian.konrad@gmail.com>
2023-11-02 16:23:21 +0100

    Add custom queries

--- src/main/java/pl/pomian/trainticketbooker/repositories/StationConnectionRepository.java
+++ src/main/java/pl/pomian/trainticketbooker/repositories/StationConnectionRepository.java
@@ -1,9 +1,41 @@
 package pl.pomian.trainticketbooker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.pomian.trainticketbooker.models.StationConnection;
 
 @Repository
 public interface StationConnectionRepository extends JpaRepository<StationConnection, Long> {
+
+    public boolean existsByFrom_NameAndTo_Name(String fromStationName, String toStationName);
+
+    public StationConnection findByFrom_NameAndTo_Name(String fromStationName, String toStationName);
+
+    public void deleteAllByTo_Name(String stationName);
+
+    public void deleteAllByFrom_Name(String stationName);
+
+    public void deleteByFrom_NameAndTo_Name(String fromStationName, String toStationName);
+
+    @Modifying
+    @Query(
+            nativeQuery = true,
+            value = "UPDATE station_connections " +
+                    "SET time_weight = ? " +
+                    "WHERE from_id = (SELECT name FROM stations WHERE id = ?) " +
+                    "AND to_id = (SELECT name FROM stations WHERE id = ?);"
+    )
+    public void updateTimeWeightByFrom_NameAndTo_Name(Integer timeWeight, String fromStationId, String toStationId);
+
+    @Modifying
+    @Query(
+            nativeQuery = true,
+            value = "UPDATE station_connections " +
+                    "SET price_weight = ? " +
+                    "WHERE from_id = (SELECT name FROM stations WHERE id = ?) " +
+                    "AND to_id = (SELECT name FROM stations WHERE id = ?);"
+    )
+    public void updatePriceWeightByFrom_NameAndTo_Name(Integer priceWeight, String fromStationId, String toStationId);
 }
--- src/main/java/pl/pomian/trainticketbooker/repositories/StationRepository.java
+++ src/main/java/pl/pomian/trainticketbooker/repositories/StationRepository.java
@@ -6,4 +6,10 @@ import pl.pomian.trainticketbooker.models.Station;
 
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
+
+    public Station findByName(String name);
+
+    public boolean existsByName(String name);
+
+    public void deleteByName(String name);
 }
