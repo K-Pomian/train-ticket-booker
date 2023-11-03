1d4b031f52223b9a83f77d708b740cdca1da3a84
K-Pomian <pomian.konrad@gmail.com>
2023-11-03 13:43:39 +0100

    Override equals and hashCode for StationDto

--- src/main/java/pl/pomian/trainticketbooker/models/StationDto.java
+++ src/main/java/pl/pomian/trainticketbooker/models/StationDto.java
@@ -1,5 +1,7 @@
 package pl.pomian.trainticketbooker.models;
 
+import java.util.Objects;
+
 public class StationDto {
 
     private String name;
@@ -14,5 +16,18 @@ public class StationDto {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StationDto that = (StationDto) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
