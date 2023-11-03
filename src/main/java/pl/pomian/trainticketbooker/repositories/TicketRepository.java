990c43c2918649d14af98855e70505935ec9670b
K-Pomian <pomian.konrad@gmail.com>
2023-11-03 15:14:30 +0100

    Add finding by UUID for TicketRepository

--- src/main/java/pl/pomian/trainticketbooker/repositories/TicketRepository.java
+++ src/main/java/pl/pomian/trainticketbooker/repositories/TicketRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.pomian.trainticketbooker.models.Ticket;
 
+import java.util.UUID;
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    Ticket findByTicketId(UUID ticketId);
 }
