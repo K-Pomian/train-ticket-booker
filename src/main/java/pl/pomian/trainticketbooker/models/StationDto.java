8a40c0d120a44784a9da211a37e1736403d48bee
K-Pomian <pomian.konrad@gmail.com>
2023-11-07 13:38:18 +0100

    Add toString impl for Station and StationDto

--- src/main/java/pl/pomian/trainticketbooker/models/StationDto.java
+++ src/main/java/pl/pomian/trainticketbooker/models/StationDto.java
@@ -19,6 +19,13 @@ public class StationDto {
     }
 
     @Override
+    public String toString() {
+        return "StationDto{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
