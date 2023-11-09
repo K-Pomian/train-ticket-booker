441314df16dddcd1dc9069cda27dcab12e8698d5
K-Pomian <pomian.konrad@gmail.com>
2023-11-09 10:21:53 +0100

    Add findStationWithConnectionsByName to StationRepository

--- src/main/java/pl/pomian/trainticketbooker/repositories/StationRepository.java
+++ src/main/java/pl/pomian/trainticketbooker/repositories/StationRepository.java
@@ -10,12 +10,16 @@ import java.util.List;
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
 
+    @Query("SELECT station from Station station JOIN FETCH station.connectedTo connections")
+    List<Station> findAllStationWithConnections();
+
     Station findByName(String name);
+
+    @Query("SELECT station FROM Station station JOIN FETCH station.connectedTo connections WHERE station.name = :name")
+    Station findStationWithConnectionsByName(String name);
 
     boolean existsByName(String name);
 
     void deleteByName(String name);
 
-    @Query("SELECT station from Station station JOIN FETCH station.connectedTo connections")
-    List<Station> findAllStationWithConnections();
 }
