ca0c2aba049729614addff9b6dfbc3fa7853f3a3
K-Pomian <pomian.konrad@gmail.com>
2023-11-09 10:22:09 +0100

    Return DTOs from StationsManagementService functions

--- src/main/java/pl/pomian/trainticketbooker/services/StationsManagementService.java
+++ src/main/java/pl/pomian/trainticketbooker/services/StationsManagementService.java
@@ -1,11 +1,14 @@
 package pl.pomian.trainticketbooker.services;
 
+import org.javatuples.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.pomian.trainticketbooker.models.Station;
 import pl.pomian.trainticketbooker.models.StationConnection;
+import pl.pomian.trainticketbooker.models.dto.StationConnectionDto;
+import pl.pomian.trainticketbooker.models.dto.StationDto;
 import pl.pomian.trainticketbooker.repositories.StationConnectionRepository;
 import pl.pomian.trainticketbooker.repositories.StationRepository;
 
@@ -27,12 +30,32 @@ public class StationsManagementService {
         this.stationConnectionRepository = stationConnectionRepository;
     }
 
-    public List<Station> getAllStations() {
-        return stationRepository.findAll();
+    public List<StationDto> getAllStations() {
+        return stationRepository.findAll().stream().map(StationDto::fromStation).toList();
     }
 
-    public Station getStationByName(String name) {
-        return stationRepository.findByName(name);
+    public List<Pair<StationDto, List<StationConnectionDto>>> getAllStationsWithConnections() {
+        List<Station> stations = stationRepository.findAllStationWithConnections();
+        return stations.stream().map(
+                        station -> new Pair<>(
+                                StationDto.fromStation(station),
+                                station.getConnectedTo().stream().map(StationConnectionDto::fromStationConnection).toList()
+                        )
+                )
+                .toList();
+    }
+
+    public StationDto getStationByName(String name) {
+        return StationDto.fromStation(stationRepository.findByName(name));
+    }
+
+    public Pair<StationDto, List<StationConnectionDto>> getStationAndConnectionsByName(String name) {
+        Station station = stationRepository.findStationWithConnectionsByName(name);
+
+        return new Pair<>(
+                StationDto.fromStation(station),
+                station.getConnectedTo().stream().map(StationConnectionDto::fromStationConnection).toList()
+        );
     }
 
     @Transactional
