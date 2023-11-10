f1e8e708e0ae322ce64b263ffa38a1eaabc25704
K-Pomian <pomian.konrad@gmail.com>
2023-11-10 19:24:29 +0100

    Add findAllByStationFrom_NameOrStationTo_NameFetchStationFromAndStationTo query to TicketRepository

--- src/main/java/pl/pomian/trainticketbooker/repositories/TicketRepository.java
+++ src/main/java/pl/pomian/trainticketbooker/repositories/TicketRepository.java
@@ -1,13 +1,18 @@
 package pl.pomian.trainticketbooker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.pomian.trainticketbooker.models.Ticket;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    @Query("SELECT ticket from Ticket ticket JOIN FETCH ticket.stationFrom connections JOIN FETCH ticket.stationTo")
+    List<Ticket> findAllByStationFrom_NameOrStationTo_NameFetchStationFromAndStationTo(String stationFromName, String stationToName);
 
     Ticket findByTicketId(UUID ticketId);
 }
