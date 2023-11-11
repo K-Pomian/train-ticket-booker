dd901376088e44bfb9f34f8ca831ed1bcb660cc5
K-Pomian <pomian.konrad@gmail.com>
2023-11-11 16:26:22 +0100

    Update getTicketById function

--- src/main/java/pl/pomian/trainticketbooker/services/TicketManagementService.java
+++ src/main/java/pl/pomian/trainticketbooker/services/TicketManagementService.java
@@ -1,12 +1,15 @@
 package pl.pomian.trainticketbooker.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.pomian.trainticketbooker.exceptions.TicketNotFoundException;
 import pl.pomian.trainticketbooker.models.Discount;
 import pl.pomian.trainticketbooker.models.Station;
 import pl.pomian.trainticketbooker.models.Ticket;
 import pl.pomian.trainticketbooker.models.User;
+import pl.pomian.trainticketbooker.models.dto.TicketDto;
 import pl.pomian.trainticketbooker.repositories.StationRepository;
 import pl.pomian.trainticketbooker.repositories.TicketRepository;
 import pl.pomian.trainticketbooker.repositories.UserRepository;
@@ -33,8 +36,15 @@ public class TicketManagementService {
         this.userRepository = userRepository;
     }
 
-    public Ticket getTicketById(UUID ticketId) {
-        return ticketRepository.findByTicketId(ticketId);
+    public TicketDto getTicketById(@NonNull UUID ticketId) throws TicketNotFoundException {
+        Ticket ticket = ticketRepository.findByTicketId(ticketId);
+
+        if (ticket == null) {
+            String message = "Ticket with id ".concat(ticketId.toString()).concat(" not found.");
+            throw new TicketNotFoundException(message);
+        }
+
+        return TicketDto.fromTicket(ticket);
     }
 
     @Transactional
